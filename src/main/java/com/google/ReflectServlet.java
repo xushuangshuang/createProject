482e00baff993cc01e26adceb3be06cb13225862
Dev--xu <xushuangshuang1@gmail.com>
2014-08-12 13:31:36 +0800

    添加了一个简单地servlet文件

---
+++ src/main/java/com/google/ReflectServlet.java
@@ -0,0 +1,17 @@
+package com.google;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import javax.servlet.ServletException;
+
+public class ReflectServlet
+{
+    public void service(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {
+
+    }
+}
+
