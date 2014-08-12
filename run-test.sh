6846d5cbb1e52f6e5ef41a36e9af8e5f520cee2d
Dev--xu <xushuangshuang1@gmail.com>
2014-08-12 15:02:50 +0800

    修改了测试脚本，打印添加的文件

--- run-test.sh
+++ run-test.sh
@@ -2,16 +2,16 @@
 
 echo "RUN-TEST START"
 
-CLASSPATH="target/classes;target/test-classes"
+CLASSPATH_RUNTIME="target/classes;target/test-classes"
 
 [[ -d lib-runtime ]] && for RUNTIMEFILE in lib-runtime/*.jar; do
 
-CLASSPATH_RUNTIME="$CLASSPATH;$RUNTIMEFILE"
+CLASSPATH_RUNTIME="$CLASSPATH_RUNTIME;$RUNTIMEFILE"
 
 done
 
-java -classpath "$CLASSPATH_RUNTIME" com.util.XuShuangShuangJunit "com.xu.ReflectServletTest"
+echo "$CLASSPATH_RUNTIME"
 
-RETVAL=$?
+java -classpath "$CLASSPATH_RUNTIME" com.util.XuShuangShuangJunit "com.xu"
 
 echo "RUN-TEST DONE"
