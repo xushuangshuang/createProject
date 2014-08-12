7e18042f7ec569bcb84590b3ff8d86b65175aadd
Dev--xu <xushuangshuang1@gmail.com>
2014-08-12 13:32:17 +0800

    添加了对测试案例类的添加

---
+++ src/test/java/com/xu/ReflectServletTest.java
@@ -0,0 +1,11 @@
+package com.xu;
+
+import com.util.XuShuangShuangJunit;
+
+public class ReflectServletTest extends XuShuangShuangJunit
+{
+    public void test_benben()
+    {
+        assertEquals("benben", "benben");
+    }
+}
