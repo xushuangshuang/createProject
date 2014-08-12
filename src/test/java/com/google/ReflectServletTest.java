b3619482aabc7064752b3b79943a49d18565b94b
Dev--xu <xushuangshuang1@gmail.com>
2014-08-12 22:52:18 +0800

    添加了新的测试案例将/reflect/contact/

--- src/test/java/com/google/ReflectServletTest.java
+++ src/test/java/com/google/ReflectServletTest.java
@@ -4,14 +4,21 @@ import com.util.XuShuangShuangJunit;
 
 public class ReflectServletTest extends XuShuangShuangJunit
 {
-    public void test_分解指定的Uri依照斜杠()
+    public void test_分解不以斜杠结尾的Uri依照斜杠()
     {
         ReflectServlet reflectServlet = new ReflectServlet();
         String[] uriParts = reflectServlet.resolveURI("/reflect/contact");
         assertEquals("reflect", uriParts[1]);
         assertEquals("contact", uriParts[2]); 
     }
-
+    
+    public void test_分解以斜杠结尾的Uri依照斜杠划分()
+    {
+        ReflectServlet reflectServlet = new ReflectServlet();
+        String[] uriParts = reflectServlet.resolveURI("/reflect/contact/");
+        assertEquals("reflect", uriParts[1]);
+        assertEquals("contact", uriParts[2]); 
+    }
     public void test_将获取到的类名首字母大写()
     {
         ReflectServlet reflectServlet = new ReflectServlet();
